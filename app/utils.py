8042edad9e4237b86a0701769a61bf8870ec0e5d
Leopoldo Rivas <leo@MacBook-Air-de-Leopoldo.local>
2024-07-28 23:23:49 -0400

    157Upate reservation logic court at the en42d

--- app/utils.py
+++ app/utils.py
@@ -1,74 +1,48 @@
 from datetime import datetime, time, timedelta
 from app.models import Court, Reservation
+from app import app, db
+from sqlalchemy import and_
 
-def get_available_times(date, court_id, use_type):
-    weekday_hours = [(8, 30), (23, 0)]
-    saturday_hours = [(8, 30), (18, 0)]
-    sunday_hours = [(8, 30), (13, 0)]
+def get_available_times(date):
+    app.logger.info(f"Obteniendo tiempos disponibles para: date={date}")
+    
+    weekday_hours = (8, 30), (23, 0)
+    saturday_hours = (8, 30), (18, 0)
+    sunday_hours = (8, 30), (13, 0)
 
-    if date.weekday() < 5:  # Lunes a viernes
+    if date.weekday() < 5:  # Monday to Friday
         opening_hour, closing_hour = weekday_hours
-    elif date.weekday() == 5:  # Sábado
+    elif date.weekday() == 5:  # Saturday
         opening_hour, closing_hour = saturday_hours
-    else:  # Domingo
+    else:  # Sunday
         opening_hour, closing_hour = sunday_hours
 
-    times = []
     current_time = datetime.combine(date, time(hour=opening_hour[0], minute=opening_hour[1]))
     closing_time = datetime.combine(date, time(hour=closing_hour[0], minute=closing_hour[1]))
 
+    times = []
     while current_time + timedelta(minutes=60) <= closing_time:
         times.append(current_time.time())
-        current_time += timedelta(minutes=5)
+        current_time += timedelta(minutes=15)  # Increment by 15 minutes
 
-    if court_id:
-        reservations = Reservation.query.filter_by(date=date, court_id=court_id).all()
-        reserved_times = [(datetime.combine(date, r.start_time), datetime.combine(date, r.end_time)) for r in reservations]
-        available_times = [t for t in times if all(not (start <= datetime.combine(date, t) < end) for start, end in reserved_times)]
-        return available_times
-
+    app.logger.info(f"Tiempos disponibles encontrados: {len(times)}")
     return times
 
-def check_availability(date, start_time, end_time):
-    reservations = Reservation.query.filter_by(date=date).all()
-    available_courts = Court.query.all()
-    
-    for reservation in reservations:
-        if not (start_time >= reservation.end_time or end_time <= reservation.start_time):
-            available_courts = [court for court in available_courts if court.id != reservation.court_id]
-
-    return available_courts
-
-
 def get_available_courts(date, start_time, use_type):
-    print(f"Buscando canchas disponibles para: date={date}, start_time={start_time}, use_type={use_type}")
+    app.logger.info(f"Buscando canchas disponibles para: date={date}, start_time={start_time}, use_type={use_type}")
 
-    # Convertir la fecha y hora a objetos datetime
-    date = datetime.strptime(date, '%Y-%m-%d').date()
-    start_time = datetime.strptime(start_time, '%H:%M').time()
+    duration = 90 if use_type in ['amistoso', 'liga'] else 60
+    end_time = (datetime.combine(date, start_time) + timedelta(minutes=duration)).time()
 
-    # Calcular la hora de término basada en el tipo de uso
-    if use_type in ['amistoso', 'liga']:
-        end_time = (datetime.combine(date, start_time) + timedelta(minutes=90)).time()
-    else:
-        end_time = (datetime.combine(date, start_time) + timedelta(minutes=60)).time()
+    # Obtener las canchas ocupadas
+    occupied_courts = db.session.query(Reservation.court_id).filter(
+        Reservation.date == date,
+        ~((Reservation.end_time <= start_time) | (Reservation.start_time >= end_time))
+    ).distinct()
 
-    # Obtener todas las reservas en la fecha y hora seleccionada
-    reservations = Reservation.query.filter_by(date=date).all()
+    # Obtener solo las canchas disponibles
+    available_courts = Court.query.filter(~Court.id.in_(occupied_courts)).all()
 
-    # Filtrar las canchas ocupadas en ese horario
-    occupied_courts = set()
-    for reservation in reservations:
-        if not (reservation.end_time <= start_time or reservation.start_time >= end_time):
-            occupied_courts.add(reservation.court_id)
-
-    # Obtener todas las canchas
-    all_courts = Court.query.all()
-
-    # Filtrar las canchas disponibles
-    available_courts = [court for court in all_courts if court.id not in occupied_courts]
-    print(f"Canchas ocupadas: {occupied_courts}")
-    print(f"Canchas disponibles: {available_courts}")
-
+    app.logger.info(f"Canchas disponibles: {[c.id for c in available_courts]}")
 
     return available_courts
